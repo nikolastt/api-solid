46164be0bf5f66fa52039338736ba46891cb6838
nikolas <nikolas.bitencourt@4mti.com.br>
2024-08-19 16:51:27 -0300

    att

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,12 +1,20 @@
 import { describe, expect, it } from "vitest"
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository"
 import { AuthenticateUseCase } from "./authenticate"
+import { hash } from "bcryptjs"
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error"
 
 describe("Authenticate Use Case", () => {
 
     it("should be able to authenticate", async () => {
         const usersRepository = new InMemoryUsersRepository()
         const sut = new AuthenticateUseCase(usersRepository)
+
+        await usersRepository.create({
+            name: "John Doe",
+            email: "johndoe@example.com",
+            password_hash: await hash("123456", 6)
+        })
 
         const { user } = await sut.execute({
             email: "johndoe@example.com",
@@ -16,5 +24,31 @@ describe("Authenticate Use Case", () => {
 
 
         expect(user.id).toEqual(expect.any(String))
+    })
+
+    it("shoud not be able to authenticate with wrong email", async () => {
+        const usersRepository = new InMemoryUsersRepository()
+        const sut = new AuthenticateUseCase(usersRepository)
+
+        await expect(() => sut.execute({
+            email: "johndoe@example.com",
+            password: "123456"
+        })).rejects.toBeInstanceOf(InvalidCredentialsError)
+    })
+
+    it("shoud not be able to authenticate with wrong password", async () => {
+        const usersRepository = new InMemoryUsersRepository()
+        const sut = new AuthenticateUseCase(usersRepository)
+
+        await usersRepository.create({
+            name: "John Doe",
+            email: "johndoe@example.com",
+            password_hash: await hash("123456", 6)
+        })
+
+        await expect(() => sut.execute({
+            email: "johndoe@example.com",
+            password: "wrong"
+        })).rejects.toBeInstanceOf(InvalidCredentialsError)
     })
 })
